b1ca75dff822aa16b23967f87c0bf917f8fa5850
Covenant <ediomoemmaeffiong@gmail.com>
2024-08-10 16:15:57 +0100

    Adding funtionalities to support translation from Jungle Language to English

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static String word, rWord;
     public static char letter;
+    public static String [] wordTwo;
 
     public static void main(String[] args) {
 
@@ -15,6 +16,15 @@ public class Main {
 
 
         String tWord = "";
+
+        if (word.charAt(1) == 'a' && (word.charAt(2) == '1' || word.charAt(2) == '2' || word.charAt(2) == '3' || word.charAt(2) == '4' || word.charAt(2) == '5')) {
+            translateTwo();
+        } else if ((word.charAt(0) == '1' || word.charAt(0) == '2' || word.charAt(0) == '3' || word.charAt(0) == '4' || word.charAt(0) == '5') && word.charAt(2) == 'a') {
+            translateTwo();
+        } else if ((word.charAt(0) == '1' || word.charAt(0) == '2' || word.charAt(0) == '3' || word.charAt(0) == '4' || word.charAt(0) == '5') && (word.charAt(1) == '1' || word.charAt(1) == '2' || word.charAt(1) == '3' || word.charAt(1) == '4' || word.charAt(1) == '5') && word.charAt(3) == 'a') {
+            translateTwo();
+        }
+
         for (int i = 0; i < word.length(); i++) {
             letter = word.charAt(i);
             translate(letter);
@@ -30,6 +40,9 @@ public class Main {
 
     public static void translate(char letter) {
         switch (letter) {
+            case ' ' -> {
+                rWord = " ";
+            }
             case 'u' -> {
                 letter = '5';
                 rWord = String.valueOf(letter);
@@ -55,6 +68,14 @@ public class Main {
             }
         }
 
+    }
+
+    public static void translateTwo() {
+        /*switch(letter) {
+            default -> {
+                rWord = letter.;
+            }
+        }*/
     }
 
 
