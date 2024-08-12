f5ba6c4d7ace11dde1ddd8eabe7bb81a61036f1d
Covenant <ediomoemmaeffiong@gmail.com>
2024-08-12 16:20:09 +0100

    Working on translating from JL

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static String word, rWord;
     public static char letter;
-    public static String [] wordTwo;
+    public static String[] jlWord = new String[50];
 
     public static void main(String[] args) {
 
@@ -17,15 +17,42 @@ public class Main {
 
         String tWord = "";
 
-        if (word.charAt(1) == 'a' && (word.charAt(2) == '1' || word.charAt(2) == '2' || word.charAt(2) == '3' || word.charAt(2) == '4' || word.charAt(2) == '5')) {
+        /*if (word.charAt(1) == 'a' && (word.charAt(2) == '1' || word.charAt(2) == '2' || word.charAt(2) == '3' || word.charAt(2) == '4' || word.charAt(2) == '5')) {
+
             translateTwo();
         } else if ((word.charAt(0) == '1' || word.charAt(0) == '2' || word.charAt(0) == '3' || word.charAt(0) == '4' || word.charAt(0) == '5') && word.charAt(2) == 'a') {
             translateTwo();
         } else if ((word.charAt(0) == '1' || word.charAt(0) == '2' || word.charAt(0) == '3' || word.charAt(0) == '4' || word.charAt(0) == '5') && (word.charAt(1) == '1' || word.charAt(1) == '2' || word.charAt(1) == '3' || word.charAt(1) == '4' || word.charAt(1) == '5') && word.charAt(3) == 'a') {
             translateTwo();
-        }
+        }*/
 
-        for (int i = 0; i < word.length(); i++) {
+        for (int j = 0; j < word.length(); j++) {
+            if (word.charAt(j+1) == 'a') {
+                jlWord[j] = String.valueOf(word.charAt(j));
+                j++;
+            } else if (word.charAt(j) == '1' || word.charAt(j) == '2' || word.charAt(j) == '3' || word.charAt(j) == '4' || word.charAt(j) == '5') {
+                switch (word.charAt(j)) {
+                    case '1' -> {
+                        jlWord[j-1] = "a";
+                    }
+                    case '2' -> {
+                        jlWord[j-1] = "b";
+                    }
+                    case '3' -> {
+                        jlWord[j-1] = "c";
+                    }
+                    case '4' -> {
+                        jlWord[j-1] = "d";
+                    }
+                    case '5' -> {
+                        jlWord[j-1] = "e";
+                    }
+                }
+            }
+        }
+        System.out.println(jlWord[0] + jlWord[1] + jlWord[2]);
+
+        /*for (int i = 0; i < word.length(); i++) {
             letter = word.charAt(i);
             translate(letter);
             tWord = tWord.concat(rWord);
@@ -33,7 +60,7 @@ public class Main {
 
         System.out.println(word + " | " + tWord);
 
-        tryAgain();
+        tryAgain();*/
 
 
     }
